8902e523816348e0c21ecd77eab7b812138c616c
Edgard3033 <ed233094@outlook.com>
2023-06-22 12:00:25 -0400

    routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 //routers for adapters
 router.use("/users", require("./users"));
-router.use("/orders", require("./orders.js"));
+router.use("/orders", require("./orders"));
 
 // GET /api/health
 router.get("/health", (req, res, next) => {
