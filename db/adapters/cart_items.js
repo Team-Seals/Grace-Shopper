3481f8d88df7809cbcbe48180f5c8d623405cf80
Mark Howell <markedhowell@gmail.com>
2023-06-24 17:16:07 -0600

    adapters cart items question json agg?

--- db/adapters/cart_items.js
+++ db/adapters/cart_items.js
@@ -18,15 +18,56 @@ async function createCartItem({ order_id, product_id, quantity, price }) {
     throw error;
   }
 }
-async function deleteCartItem(order_id, product_id){
-
-};
-async function editCartItem(order_id, product_id, quantity){
-
-};
-async function getCartItems(order_id){
-
-};
+async function deleteCartItem(order_id, product_id) {
+  try {
+    console.log("deleting cart item");
+    const {
+      rows: [cart_items],
+    } = await client.query(
+      `DELETE FROM cart_items
+      WHERE id = $1, $2
+      RETURNING *;
+      `,
+      [order_id, product_id]
+    );
+    return cart_items;
+  } catch (error) {
+    throw error;
+  }
+}
+async function editCartItem(order_id, product_id, { quantity }) {
+  try {
+    console.log("...editing cart item");
+    const {
+      rows: [cart_items],
+    } = await client.query(
+      `UPDATE cart_items
+      SET quantity = 1$
+      WHERE id=$2
+      RETURNING *;
+      `,
+      [quantity]
+    );
+    return cart_items;
+  } catch (error) {
+    throw error;
+  }
+}
+async function getCartItems(order_id) {
+  try {
+    console.log("getting cart items");
+    const {
+      rows: [cart_items],
+    } = await client.query(
+      `SELECT *
+      FROM cart_items;
+      `
+    );
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
 module.exports = {
   createCartItem,
 };
