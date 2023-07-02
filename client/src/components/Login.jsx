482eb1d76cc9c11c7d25d3e92710230422900063
Edgard3033 <ed233094@outlook.com>
2023-07-02 13:59:53 -0400

    Login and Register are working

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../api/auth";
+import useAuth from "../hooks/useAuth";
 
 export default function Login() {
+  const { setLoggedIn } = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!username || !password) {
+      console.log("Please enter username and password");
+      return;
+    }
+    try {
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      console.log("response in Login:", response);
+
+      if (response.ok) {
+        //successful login
+        setLoggedIn(true);
+        console.log("Login is a success!");
+        navigate("/");
+      } else {
+        //Login failed, display an error message to the user
+        console.log("Invalid username or password");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
-      <h1>LOGIN!!</h1>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Submit</button>
+        <Link to="/register">Don't have an account? Register now!</Link>
+      </form>
     </div>
   );
 }
---
+++ client/src/components/auth/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+import { fetchMe } from "../../api/auth";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({ id: null, username: "Guest" });
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    async function getMe() {
+      try {
+        const { user } = await fetchMe();
+        setUser(user);
+      } catch (error) {
+        setUser({ username: "Guest" });
+      }
+    }
+    getMe();
+  }, []);
+
+  const contextValue = {
+    user,
+    setUser,
+    loggedIn,
+    setLoggedIn,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
