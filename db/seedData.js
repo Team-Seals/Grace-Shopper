938c19b6c0565234fc69408a5effb1a3d5e916d7
Edgard3033 <ed233094@outlook.com>
2023-07-11 18:26:02 -0400

    shoe pictures

--- db/seedData.js
+++ db/seedData.js
@@ -17,6 +17,8 @@ const products = [
   {
     title: "Nike Air Force 1",
     description: "Padded collar feels soft and comfortable",
+    image_url:
+      "https://tse2.mm.bing.net/th?id=OIP.CpH6AkieN2m8qqaK8z3LWwHaHa&pid=Api&P=0&h=180",
     price: 120,
     inventory: 30,
     category_id: 1,
@@ -24,6 +26,7 @@ const products = [
   {
     title: "Nike Trainers",
     description: "Lightweight training shoes",
+    image_url: "",
     price: 70,
     inventory: 50,
     category_id: 1,
@@ -31,6 +34,7 @@ const products = [
   {
     title: "Adidas Originals Superstar",
     description: "Original superstarts is casual and comfortable",
+    image_url: "",
     price: 45,
     inventory: 20,
     category_id: 2,
@@ -39,6 +43,7 @@ const products = [
     title: "Advantage Sneaker",
     description:
       "Adidas Advantage sneakers are comforatable and lightweight everywhere and anywhere",
+    image_url: "",
     price: 30,
     inventory: 100,
     category_id: 2,
@@ -46,6 +51,7 @@ const products = [
   {
     title: "Yeezy Slides",
     description: "Slides from popular rapper Kanye West",
+    image_url: "",
     price: 200,
     inventory: 200,
     category_id: 2,
@@ -53,6 +59,7 @@ const products = [
   {
     title: "Puma Roma Basic",
     description: "Classic casual Puma Sneakers for fashion",
+    image_url: "",
     price: 70,
     inventory: 100,
     category_id: 3,
@@ -60,6 +67,7 @@ const products = [
   {
     title: "Puma LaMelo Ball MB.01",
     description: "Basketball shoes from All-Star basketball player LaMelo Ball",
+    image_url: "",
     price: 114,
     inventory: 100,
     category_id: 3,
@@ -67,6 +75,7 @@ const products = [
   {
     title: "Converse Chuck Taylor All Star Classic",
     description: "Original Chuck Talyors for the gym or for casual fashion",
+    image_url: "",
     price: 65,
     inventory: 120,
     category_id: 4,
@@ -75,6 +84,7 @@ const products = [
     title: "Converse Chuck Taylor All Star Classic Lows",
     description:
       "Original Chuck Taylors Lows for the gym or for casual fashion",
+    image_url: "",
     price: 35,
     inventory: 100,
     category_id: 4,
@@ -82,6 +92,7 @@ const products = [
   {
     title: "New Balance 550",
     description: "Lifestyle / Casual shoes great for vintage fashion",
+    image_url: "",
     price: 110,
     inventory: 50,
     category_id: 5,
@@ -89,6 +100,7 @@ const products = [
   {
     title: "New Balance 2002R",
     description: "Top Quality vintage shoes great for casual fashion",
+    image_url: "",
     price: 140,
     inventory: 40,
     category_id: 5,
@@ -96,6 +108,7 @@ const products = [
   {
     title: "Vans Old Skool",
     description: "Old school vintage vans",
+    image_url: "",
     price: 50,
     inventory: 120,
     category_id: 6,
@@ -103,6 +116,7 @@ const products = [
   {
     title: "Vans Classic Slip-On",
     description: "Classic Slip-On shoes that are super comfortable",
+    image_url: "",
     price: 60,
     inventory: 100,
     category_id: 6,
@@ -110,6 +124,7 @@ const products = [
   {
     title: "Reebok Club C 85",
     description: "Vintage Chalk Essential shoes",
+    image_url: "",
     price: 90,
     inventory: 80,
     category_id: 7,
@@ -117,6 +132,7 @@ const products = [
   {
     title: "Reebok Classic Nylon Sneaker",
     description: "Classic comfortable sneakers, great for casual or running",
+    image_url: "",
     price: 60,
     inventory: 60,
     category_id: 7,
@@ -125,6 +141,7 @@ const products = [
     title: "Crocs Classic Clog",
     description:
       "Classic Croc Clogs that are super comfortable and lightweight",
+    image_url: "",
     price: 25,
     inventory: 100,
     category_id: 8,
@@ -132,6 +149,7 @@ const products = [
   {
     title: "Crocs Classic Lined Clog",
     description: "Lined Classic Crocs to keep feet warm and cozy",
+    image_url: "",
     price: 60,
     inventory: 80,
     category_id: 8,
